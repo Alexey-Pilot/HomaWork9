29b5936c44e14916aad43ecce70d3d13246a0f1a
Alexey <alexragnarek@gmail.com>
2023-02-10 19:52:19 +0500

    ex 68 is comlited

---
+++ ex 68/Program.cs
@@ -0,0 +1,22 @@
+﻿
+double Ackerman (double m, double n)
+{
+  if(m == 0) 
+    return n + 1;
+  if (m > 0 && n == 0) 
+    return Ackerman(m - 1, 1);
+  if (m > 0 && n > 0) 
+    return Ackerman(m - 1, Ackerman(m, n - 1));
+    
+  return Ackerman(m, n);
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+double m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+double n = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(Ackerman(m, n));
